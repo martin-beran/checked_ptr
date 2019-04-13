e47b9726afb0252e26a3c3d4c894059b7537ebbb
Martin Beran <martin.beran@avast.com>
2019-04-14 01:37:57 +0200

    variants SHARED_MUTEX_PTR, SHARED_RWLOCK_PTR, fixed CHECKED_RAW_PTR

--- examples/benchmark.cpp
+++ examples/benchmark.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <shared_mutex>
 #include <string>
 #include <string_view>
 #include <thread>
@@ -16,7 +17,7 @@ struct shared_data {
     long long val;
 };
 
-#ifdef SHARED_PTR
+#if defined(SHARED_PTR)||defined(SHARED_MUTEX_PTR)||defined(SHARED_RWLOCK_PTR)
 using thread_data = std::shared_ptr<shared_data>;
 #else
 using thread_data = cptr::master_ptr<shared_data>;
@@ -24,70 +25,98 @@ using thread_data = cptr::master_ptr<shared_data>;
 
 auto make_data(long long val = 0)
 {
-#ifdef SHARED_PTR
+#if defined(SHARED_PTR)||defined(SHARED_MUTEX_PTR)||defined(SHARED_RWLOCK_PTR)
     return std::make_shared<shared_data>(val);
 #else
     return std::make_shared<cptr::checked_object<shared_data>>(val);
 #endif
 }
 
+#ifdef SHARED_MUTEX_PTR
+std::mutex mtx;
+#endif
+#ifdef SHARED_RWLOCK_PTR
+std::shared_mutex mtx;
+#endif
+
 void writer_fun(thread_data& data, long long iter,
                 long long w_iter)
 {
     [[maybe_unused]] volatile long long target = 0;
-#ifndef SHARED_PTR
 #ifdef CHECKED_WEAK_PTR
     cptr::checked_weak_ptr<shared_data> ptr{data};
-#else
+#elif defined(CHECKED_SHARED_PTR)||defined(CHECKED_RAW_PTR)
     cptr::checked_shared_ptr<shared_data> ptr{data};
-#endif
 #endif
     for (long long i = 0; i < iter; ++i) {
         if (i % w_iter == 0) {
 #ifdef SHARED_PTR
             std::atomic_store_explicit(&data, make_data(i),
                                        std::memory_order_release);
+#elif defined(SHARED_MUTEX_PTR)||defined(SHARED_RWLOCK_PTR)
+            std::lock_guard lck(mtx);
+            data = make_data(i);
 #else
             data.set(make_data(i));
 #endif
         }
 #ifdef SHARED_PTR
         auto p = std::atomic_load_explicit(&data, std::memory_order_acquire);
-#else
-#ifdef RAW_PTR
-        auto p = ptr.get_raw();
-#else
-        auto p = ptr.get_shared();
 #endif
+#ifdef SHARED_MUTEX_PTR
+        std::lock_guard lck(mtx);
+        auto& p = data;
+#endif
+#ifdef SHARED_RWLOCK_PTR
+        std::shared_lock lck(mtx);
+        auto& p = data;
+#endif
+#ifdef CHECKED_RAW_PTR
+        auto p = ptr.get_raw();
+#endif
+#if defined(CHECKED_SHARED_PTR)||defined(CHECKED_WEAK_PTR)
+        auto p = ptr.get_shared();
 #endif
         if (p)
             target = p->val;
     }
+    static std::mutex cout_mtx;
+    std::lock_guard cout_lck(cout_mtx);
+    std::cout << "target=" << target << std::endl;
 }
 
 void reader_fun(const thread_data& data, long long iter)
 {
     [[maybe_unused]] volatile long long target = 0;
-#ifndef SHARED_PTR
 #ifdef CHECKED_WEAK_PTR
     cptr::checked_weak_ptr<shared_data> ptr{data};
-#else
+#elif defined(CHECKED_SHARED_PTR)||defined(CHECKED_RAW_PTR)
     cptr::checked_shared_ptr<shared_data> ptr{data};
-#endif
 #endif
     for (long long i = 0; i < iter; ++i) {
 #ifdef SHARED_PTR
         auto p = std::atomic_load_explicit(&data, std::memory_order_acquire);
-#else
-#ifdef RAW_PTR
-        auto p = ptr.get_raw();
-#else
-        auto p = ptr.get_shared();
 #endif
+#ifdef SHARED_MUTEX_PTR
+        std::lock_guard lck(mtx);
+        auto& p = data;
+#endif
+#ifdef SHARED_RWLOCK_PTR
+        std::shared_lock lck(mtx);
+        auto& p = data;
+#endif
+#ifdef CHECKED_RAW_PTR
+        auto p = ptr.get_raw();
+#endif
+#if defined(CHECKED_SHARED_PTR)||defined(CHECKED_WEAK_PTR)
+        auto p = ptr.get_shared();
 #endif
         if (p)
             target = p->val;
     }
+    static std::mutex cout_mtx;
+    std::lock_guard cout_lck(cout_mtx);
+    std::cout << "target=" << target << std::endl;
 }
 
 template <class T> void ston(T& n, const std::string& s);
@@ -123,6 +152,12 @@ try {
     std::cout << "variant " <<
 #ifdef SHARED_PTR
         "shared_ptr"
+#endif
+#ifdef SHARED_MUTEX_PTR
+        "shared_mutex"
+#endif
+#ifdef SHARED_RWLOCK_PTR
+        "shared_rwlock"
 #endif
 #ifdef CHECKED_SHARED_PTR
         "checked_shared_ptr"
